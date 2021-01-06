e62375d888df8152746a7d3dc2a6e039e592313d
Sangbum Kim <sbkim@surromind.ai>
2021-01-06 17:08:46 +0900

    feature: support transform

--- pachy_dataset.py
+++ pachy_dataset.py
@@ -5,6 +5,7 @@ import python_pachyderm
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
 
 from .utils import *
 
@@ -17,7 +18,7 @@ class PachyClassificationDataset(Dataset):
 
   def __init__(self, commit, path_prefix="/",
                pachy_host="175.197.4.150", pachy_port="30650",
-               local_root='/data'):
+               local_root='/data', transform=ToTensor()):
     self.commit = commit
     self.path_prefix = path_prefix
     self.client = python_pachyderm.Client(host=pachy_host, port=pachy_port)
@@ -25,6 +26,7 @@ class PachyClassificationDataset(Dataset):
     self.class_labels = list(set(get_class_label_from_path(path, path_prefix)
                                  for path in self.path_lst))
     self.num_classes = len(self.class_labels)
+    self.transform = transform
 
     self.local_root = local_root
     self._download_data_from_pachyderm()
@@ -48,7 +50,7 @@ class PachyClassificationDataset(Dataset):
 
     path = self.file_lst[idx]
     return (
-      Image.open(os.path.join(self.local_root, path)),
+      self.transform(Image.open(os.path.join(self.local_root, path))),
       index_to_one_hot(
         self.class_labels.index(
           get_class_label_from_path(path, self.path_prefix)),
