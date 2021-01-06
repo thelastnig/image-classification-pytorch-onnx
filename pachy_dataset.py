7c3d64cf49c64d62631445daea357442d08d50e9
Sangbum Kim <sbkim@surromind.ai>
2021-01-06 17:03:37 +0900

    feature: download dataset before start training

--- pachy_dataset.py
+++ pachy_dataset.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import python_pachyderm
 import torch
 from PIL import Image
@@ -11,7 +14,10 @@ class PachyClassificationDataset(Dataset):
   Image Classification Dataset for Pachyderm
   Assumes class-wise folder structure as flow_from_directory
   """
-  def __init__(self, commit, path_prefix="/", pachy_host="175.197.4.150", pachy_port="30650"):
+
+  def __init__(self, commit, path_prefix="/",
+               pachy_host="175.197.4.150", pachy_port="30650",
+               local_root='/data'):
     self.commit = commit
     self.path_prefix = path_prefix
     self.client = python_pachyderm.Client(host=pachy_host, port=pachy_port)
@@ -19,6 +25,17 @@ class PachyClassificationDataset(Dataset):
     self.class_labels = list(set(get_class_label_from_path(path, path_prefix)
                                  for path in self.path_lst))
     self.num_classes = len(self.class_labels)
+
+    self.local_root = local_root
+    self._download_data_from_pachyderm()
+
+  def _download_data_from_pachyderm(self):
+    for path in self.path_lst:
+      local_path = os.path.join(self.local_root, path)
+      os.makedirs(os.path.dirname(local_path), exist_ok=True)
+      pfs_file = self.client.get_file(self.commit, path)
+      with open(local_path, 'w') as local_file:
+        shutil.copyfileobj(pfs_file, local_file)
 
   def __len__(self):
     return len(self.file_lst)
@@ -30,9 +47,8 @@ class PachyClassificationDataset(Dataset):
       return [self[i] for i in idx]
 
     path = self.file_lst[idx]
-    pfs_file = self.client.get_file(self.commit, path)
     return (
-      Image.open(pfs_file),
+      Image.open(os.path.join(self.local_root, path)),
       index_to_one_hot(
         self.class_labels.index(
           get_class_label_from_path(path, self.path_prefix)),
