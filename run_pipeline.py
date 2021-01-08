a1adfa29754db618130401bcd7ce86ce3609b35c
Sangbum Kim <sbkim@surromind.ai>
2021-01-08 13:53:03 +0900

    fix: typo in strftime

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   pipeline_name = 'trainer_classification'
-  timestamp = datetime.now().strftime("%Y%m%d_%H%%S")
+  timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 
   @dsl.pipeline(
