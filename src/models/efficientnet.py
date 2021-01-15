ae877d3a3a799e787f45b38291aa7517e6eaec8f
Sangbum Kim <sbkim@surromind.ai>
2021-01-15 13:38:56 +0900

    refactor: move models to models dir

--- src/models/efficientnet.py
+++ src/models/efficientnet.py
@@ -5,7 +5,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from .utils_effinet import (
+from src.models.utils import (
     round_filters,
     round_repeats,
     drop_connect,
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import os
 
 import torch
 
-from src.resnet import *
+from src.models.resnet import *
 from src.pachy_dataset import PachyClassificationDataset
 from src.trainer import TrainerBase
 
