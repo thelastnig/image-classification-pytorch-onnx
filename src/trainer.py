cbce64340010921aa0d66e79fc2c66583225bba6
Sangbum Kim <sbkim@surromind.ai>
2021-01-15 15:41:50 +0900

    fix: properly implement train, val, test

--- src/trainer.py
+++ src/trainer.py
@@ -9,18 +9,18 @@ from torch.utils.data import DataLoader
 from src.utils import get_optimizer, AverageMeter, accuracy
 
 
-class TrainerBase(object):
+class ImageClassificationTrainer(object):
   def __init__(self,
-               train_dataset, test_dataset, preprocessor,
+               train_dataset, val_dataset, test_dataset,
                model, hyper_dict, experiment_name,
                device):
     self.train_dataset = train_dataset
+    self.val_dataset = val_dataset
     self.test_dataset = test_dataset
-    self.preprocessor = preprocessor
 
     self.model = model
     self.best_model = copy.deepcopy(model)
-    self.best_test_loss = None
+    self.best_val_loss = None
 
     self.epochs = hyper_dict['epochs']
     self.batch_size = hyper_dict['batch_size']
@@ -30,26 +30,18 @@ class TrainerBase(object):
     self.experiment_name = experiment_name
     self.device = device
 
-    self.avg_meter = {
-      'train_loss': AverageMeter(),
-      'train_acc': AverageMeter(),
-      'train_time': AverageMeter(),
-      'test_loss': AverageMeter(),
-      'test_acc': AverageMeter(),
-      'test_time': AverageMeter(),
-    }
-    self.tag_str = {
-      'train_loss': "",
-      'train_acc': "",
-      'train_time': "",
-      'test_loss': "",
-      'test_acc': "",
-      'test_time': "",
-    }
+    key_lst = []
+    for split in ('train', 'val', 'test'):
+      for metric in ('loss', 'acc'):
+        key_lst.append(f"{split}_{metric}")
 
-    self.test_len = len(test_dataset)
+    self.avg_meter = {key: AverageMeter() for key in key_lst}
+    self.tag_str = {key: "" for key in key_lst}
+
     self.train_ldr = DataLoader(train_dataset, batch_size=self.batch_size,
                                 num_workers=self.num_workers, shuffle=True)
+    self.val_ldr = DataLoader(val_dataset, batch_size=self.batch_size,
+                              num_workers=self.num_workers, shuffle=False)
     self.test_ldr = DataLoader(test_dataset, batch_size=self.batch_size,
                                num_workers=self.num_workers, shuffle=False)
     self.optimizer = get_optimizer(model.parameters(), hyper_dict)
@@ -72,24 +64,18 @@ class TrainerBase(object):
       self.avg_meter['train_time'].update(end_time - start_time)
       self.current_iter += 1
 
-  def test(self):
+  def test(self, loader, split_str="test"):
     with torch.no_grad():
-      for x, gt in self.test_ldr:
+      for x, gt in loader:
         start_time = time.time()
         self.model.zero_grad()
         gt = gt.to(self.device)
         pred, loss = self.model(x.to(self.device), gt)
         acc = accuracy(pred, gt.argmax(dim=1), 1)
-        self.avg_meter['test_loss'].update(loss.item())
-        self.avg_meter['test_acc'].update(acc)
+        self.avg_meter[f'{split_str}_loss'].update(loss.item())
+        self.avg_meter[f'{split_str}_acc'].update(acc)
         end_time = time.time()
-        self.avg_meter['test_time'].update(end_time - start_time)
-
-  def predict(self, x):
-    x = self.preprocessor(x).to(self.device)
-    with torch.no_grad():
-      pred, _ = self.model(x)
-    return pred
+        self.avg_meter[f'{split_str}_time'].update(end_time - start_time)
 
   def save_checkpoint(self, epoch):
     filename = f"checkpoint.{epoch}.pth"
@@ -119,13 +105,13 @@ class TrainerBase(object):
       self.tag_str[key] += f"{round(self.avg_meter[key].avg, 3)} "
       mlflow.set_tag(f"{self.experiment_name}_{key}", self.tag_str[key])
 
-    for split in ('train', 'test'):
+    for split in ('train', 'val', 'test'):
       for metric in ('loss', 'acc'):
         key = f"{split}_{metric}"
         mlflow.log_metric(key=key, value=self.avg_meter[key].avg, step=epoch)
 
-    if self.best_test_loss is None or self.best_test_loss > self.avg_meter['test_loss'].avg:
-      self.best_test_loss = self.avg_meter['test_loss'].avg
+    if self.best_val_loss is None or self.best_val_loss > self.avg_meter['val_loss'].avg:
+      self.best_val_loss = self.avg_meter['val_loss'].avg
       self.best_model = copy.deepcopy(self.model)
 
   def after_train(self):
@@ -140,7 +126,8 @@ class TrainerBase(object):
     for epoch in range(self.epochs):
       self.before_epoch(epoch)
       self.train_epoch()
-      self.test()
+      self.test(self.val_ldr, "val")
+      self.test(self.test_ldr, "test")
       self.after_epoch(epoch)
 
     self.after_train()
