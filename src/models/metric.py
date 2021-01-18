299a6300c7bd5b42177881fbfaebbb3375c15f80
Sangbum Kim <sbkim@surromind.ai>
2021-01-18 15:47:48 +0900

    fix: use macro average for mean IoU

--- src/models/metric.py
+++ src/models/metric.py
@@ -4,4 +4,5 @@ from sklearn.metrics import jaccard_score
 
 def calculate_iou(pred, gt):
   return jaccard_score(pred.detach().cpu().numpy().reshape(-1),
-                       gt.detach().cpu().numpy().reshape(-1))
+                       gt.detach().cpu().numpy().reshape(-1),
+                       average="macro")
