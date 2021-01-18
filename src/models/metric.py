6e69cdc2c3dcc7256ac4752b67db9bd7ea48755b
Sangbum Kim <sbkim@surromind.ai>
2021-01-18 13:07:03 +0900

    fix: update sklearn function name

--- src/models/metric.py
+++ src/models/metric.py
@@ -1,7 +1,7 @@
 import numpy as np
-from sklearn.metrics import jaccard_similarity_score
+from sklearn.metrics import jaccard_score
 
 
 def calculate_iou(pred, gt):
-  return jaccard_similarity_score(pred.detach().cpu().numpy().reshape(-1),
-                                  gt.detach().cpu().numpy().reshape(-1))
+  return jaccard_score(pred.detach().cpu().numpy().reshape(-1),
+                       gt.detach().cpu().numpy().reshape(-1))
